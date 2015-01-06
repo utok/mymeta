ee21907ecb1f8ac780a52dc9a3f74f55d055f524
toguh <root@toguh-tondang.com>
2015-01-06 05:49:54 -0500

    theme masuk module jquery update masuk

---
+++ sites/all/themes/gratis/STARTERKIT/gruntfile.js
@@ -0,0 +1,38 @@
+var timer = require("grunt-timer");
+module.exports = function (grunt) {
+  timer.init(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        includePaths: require('node-neat').includePaths
+      }, // watch options
+      sass: {
+        files: ['sass/**/*.{scss,sass}', 'sass/_base/**/*.{scss,sass}'],
+        tasks: ['sass:render']
+      }, //sass
+      livereload: {
+        files: ['*.html', '*.php', 'templates/**/*.{tpl,php}', 'js/**/*.{js,json}', 'css/*.css', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        options: {
+          livereload: true
+        } // options
+      } // livereload
+    }, //watch
+    sass: {
+      render: {
+        options: {
+          includePaths: require('node-neat').includePaths,
+          sourceMap: true,
+          sourceComments: false,
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/subtheme-styles.css': 'sass/subtheme-styles.scss',
+        } //files
+      } // render
+    } // sass
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass:render', 'watch']);
+};
